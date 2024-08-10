eee32d47c8251317c0197a78441a892cbb09cf1d
Fron4i <ilya10306@mail.ru>
2024-08-10 23:21:38 +0300

    up parameters wss

--- server/ws-server.js
+++ server/ws-server.js
@@ -1,15 +1,25 @@
 const fs = require("fs")
 const https = require("https")
 const WebSocket = require("ws")
+const path = require("path")
 
-// Чтение сертификатов
-const server = https.createServer({
-	cert: fs.readFileSync("/etc/letsencrypt/live/car-service.fvds.ru/fullchain.pem"),
-	key: fs.readFileSync("/etc/letsencrypt/live/car-service.fvds.ru/privkey.pem"),
+const keyPath = "/etc/letsencrypt/live/car-service.fvds.ru/privkey.pem"
+const certPath = "/etc/letsencrypt/live/car-service.fvds.ru/fullchain.pem"
+
+// Создание HTTPS-сервера
+const httpsServer = https.createServer({
+	key: fs.readFileSync(keyPath),
+	cert: fs.readFileSync(certPath),
 })
 
 // Создание WebSocket-сервера, используя HTTPS-сервер
-const wss = new WebSocket.Server({ server })
+const wss = new WebSocket.Server({
+	server: httpsServer,
+	perMessageDeflate: false, // Отключение сжатия сообщений, если это нужно
+	verifyClient: (info, done) => {
+		done(true) // Всегда принимать соединение
+	},
+})
 
 // Хранилище отложенных ответов
 const pendingResponses = new Map()
@@ -86,6 +96,6 @@ wss.on("connection", (ws) => {
 })
 
 // Запуск HTTPS-сервера и WebSocket-сервера на одном порту
-server.listen(8081, () => {
+httpsServer.listen(8081, () => {
 	console.log("WebSocket сервер запущен на wss://car-service.fvds.ru:8081")
 })
