61083005b764e19ac5c96da248e94b3ddc5681f5
Fron4i <ilya10306@mail.ru>
2024-08-11 01:43:54 +0300

    up database.js

--- bd/database.js
+++ bd/database.js
@@ -1,35 +1,48 @@
 const { Sequelize, DataTypes } = require("sequelize")
 
-// Create the Sequelize connection with logging disabled
+// Создание подключения к базе данных
 const sequelize = new Sequelize({
 	dialect: "sqlite",
 	storage: "database.sqlite",
-	logging: false, // Disable logging
+	logging: false, // Отключение логирования
 })
 
-// Определение модели пользователя
-const User = sequelize.define("User", {
-	id: {
-		type: DataTypes.INTEGER,
-		primaryKey: true,
+// Определение модели пользователя с явным указанием имени таблицы
+const User = sequelize.define(
+	"User",
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+		},
+		first_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		last_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		token: {
+			type: DataTypes.STRING,
+		},
+		tokenExpires: {
+			type: DataTypes.DATE,
+		},
 	},
-	first_name: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	last_name: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	token: {
-		type: DataTypes.STRING,
-	},
-	tokenExpires: {
-		type: DataTypes.DATE,
-	},
-})
+	{
+		tableName: "Users", // Указываем имя таблицы в базе данных
+	}
+)
 
-// Синхронизация базы данных (создание таблицы, если ее нет)
-sequelize.sync()
+// Синхронизация базы данных (создание таблицы, если ее нет, и обновление структуры)
+sequelize
+	.sync({ alter: true }) // alter: true для изменения существующей таблицы
+	.then(() => {
+		console.log("Database & tables created/updated!")
+	})
+	.catch((error) => {
+		console.error("Error creating/updating database:", error)
+	})
 
 module.exports = { User, sequelize }
